8967dbefb968109c75a8700a843b6b3281fe73e7
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-03-22 15:11:19 +0000

    added js html and css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Nathan Araujo All rights reserved
+// Created by: Nathan Araujo
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calculateArea () {
+  // this function calculates area
+  document.getElementById('multiply-math').innerHTML = 'The area is ' + (7 * 4) + ' cm²'
+}
+
+function calculatePerimeter () {
+  // this function calculates perimeter
+  document.getElementById('add-math').innerHTML = 'The perimeter is ' + (7 * 2 + 4 * 2) + ' cm'
+}
